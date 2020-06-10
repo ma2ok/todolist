38e9fd281f269f7b5f08f9f83a40340399b1c0bb
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-10 16:28:56 +0900

    HTMLとPass変更

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ ini_set('display_errors', 1);
 
 define('DSN', 'mysql:host=localhost;dbname=dotinstall_todo_app');
 define('DB_USERNAME', 'dbuser');
-define('DB_PASSWORD', '$6Ydnt7p');
+define('DB_PASSWORD', '0ikimono');
--- index.php
+++ index.php
@@ -4,3 +4,31 @@ require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/functions.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+</head>
+<body>
+  <div id="container">
+    <h1>Todos</h1>
+    <form action="">
+      <input type="text" placeholder="What needs to be done?">
+    </form>
+    <ul>
+      <li>
+        <input type="checkbox">
+        <span>Do something</span>
+        <div>x</div>
+      </li>
+      <li>
+        <input type="checkbox" checked>
+        <span>Do something again!</span>
+        <div>x</div>
+      </li>
+    </ul>
+  </div>
+</body>
+</html>
