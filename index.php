ae64c1ee39a8772a793f2534e739ba45bbec4ae9
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-10 16:44:22 +0900

    とりあえずスタイル調整

--- index.php
+++ index.php
@@ -9,24 +9,26 @@ require_once(__DIR__ . '/functions.php');
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title>My Todos</title>
+  <link rel="stylesheet" href="styles.css">
+  <title>Todo List</title>
 </head>
 <body>
   <div id="container">
-    <h1>Todos</h1>
+    <h1>Todoリスト</h1>
     <form action="">
-      <input type="text" placeholder="What needs to be done?">
+      <input type="text">
+      <input type= "submit" value= "投稿">
     </form>
     <ul>
       <li>
         <input type="checkbox">
         <span>Do something</span>
-        <div>x</div>
+        <div class="delete_todo">x</div>
       </li>
       <li>
         <input type="checkbox" checked>
-        <span>Do something again!</span>
-        <div>x</div>
+        <span class="done">Do something again!</span>
+        <div class="delete_todo">x</div>
       </li>
     </ul>
   </div>
