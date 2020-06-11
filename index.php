28e4a870db3f88062ab6608dc94c2d7c3b194799
Tubasa Matsuoka <workwing0283@gamil.com>
2020-06-11 10:38:13 +0900

    Todoリストを修正

--- index.php
+++ index.php
@@ -2,34 +2,38 @@
 
 require_once(__DIR__ . '/config.php');
 require_once(__DIR__ . '/functions.php');
+require_once(__DIR__ . '/Todo.php');
+
+// get todos
+$todoApp = new \MyApp\Todo();
+$todos = $todoApp->getAll();
+
+// var_dump($todos);
+// exit;
 
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
+  <title>Todoリスト</title>
   <link rel="stylesheet" href="styles.css">
-  <title>Todo List</title>
 </head>
 <body>
   <div id="container">
-    <h1>Todoリスト</h1>
+    <h1>Todosリスト</h1>
     <form action="">
-      <input type="text">
+      <input type="text" id="new_todo" placeholder="">
       <input type= "submit" value= "投稿">
     </form>
     <ul>
+    <?php foreach ($todos as $todo) : ?>
       <li>
-        <input type="checkbox">
-        <span>Do something</span>
+        <input type="checkbox" <?php if ($todo->state === '1') { echo 'checked'; } ?>>
+        <span class="<?php if ($todo->state === '1') { echo 'done'; } ?>"><?= h($todo->title); ?></span>
         <div class="delete_todo">x</div>
       </li>
-      <li>
-        <input type="checkbox" checked>
-        <span class="done">Do something again!</span>
-        <div class="delete_todo">x</div>
-      </li>
+    <?php endforeach; ?>
     </ul>
   </div>
 </body>
